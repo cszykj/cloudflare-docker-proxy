743251ee29dd2638417a577393ab67d9c0913518
rako <98137160+cszykj@users.noreply.github.com>
2024-07-10 13:24:21 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.zhi-yue.com": dockerHub,
+  "quay.zhi-yue.com": "https://quay.io",
+  "gcr.zhi-yue.com": "https://gcr.io",
+  "k8s-zhi-yue.com.so": "https://k8s.gcr.io",
+  "k8s.zhi-yue.com": "https://registry.k8s.io",
+  "ghcr.zhi-yue.com": "https://ghcr.io",
+  "cloudsmith.zhi-yue.com": "https://docker.cloudsmith.io",
+  "ecr.zhi-yue.com": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.zhi-yue.com": dockerHub,
 };
 
 function routeByHosts(host) {
